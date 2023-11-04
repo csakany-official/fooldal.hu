f120e45faea31916f2ee28cff601704c5c6a53db
csakany-official <149701656+csakany-official@users.noreply.github.com>
2023-11-04 14:39:20 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -26,3 +26,16 @@ function toggleTheme() {
 
     document.getElementById('theme-style').setAttribute('href', newTheme);
 }
+
+setCookie = (cName, cValue, expadys) => {
+    let date = new Date();
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookies = cName + "=" + cValue + "; " + expires + "; path=/";
+}
+
+
+document.querySelector("#cookies-btn").addEventListener("click", () => {
+    document.querySelector("#cookies").style.display = "none";
+    setCookie("cookie", true, 30)
+})
